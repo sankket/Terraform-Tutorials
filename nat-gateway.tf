27dba8ed89facf32c96fbc0fb262d430c9fea103
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-04-07 19:57:19 +0530

    code updated

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,1 +1,31 @@
+# CREATE ELASTIC IP ADDRESS FOR NAT GATEWAY
 
+  resource "aws_eip" "dfsc-nat1" {
+}
+  resource "aws_eip" "dfsc-nat2" {
+}
+  
+
+# CREATE NAT GATEWAY in EU-West-1A
+
+  resource "aws_nat_gateway" "dfsc-nat-gateway-1a" {
+  allocation_id = aws_eip.dfsc-nat1.id
+  subnet_id     = aws_subnet.dfsc-public-1a.id
+
+  tags = {
+    Name        = "Nat Gateway-1a"
+    Terraform   = "True"
+  }
+}
+
+# CREATE NAT GATEWAY in EU-West-1B
+
+resource "aws_nat_gateway" "dfsc-nat-gateway-1b" {
+  allocation_id = aws_eip.dfsc-nat2.id
+  subnet_id     = aws_subnet.dfsc-public-1b.id
+
+  tags = {
+    Name        = "Nat Gateway-1b"
+    Terraform   = "True"
+  }
+}
