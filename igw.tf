61a844d03fef874fef7d1763988caadc4cb0e92c
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-04-07 19:27:55 +0530

    Code Updated.

--- igw.tf
+++ igw.tf
@@ -1,1 +1,10 @@
+# Create and Attach internet gateway
 
+resource "aws_internet_gateway" "dfsc-igw" {
+  #we need to attach internet gateway to vpc.
+  vpc_id = aws_vpc.dfsc_vpc.id
+  tags = {
+    Name        = "DFSC Internet Gateway"
+    Terraform   = "true"
+  }
+}
