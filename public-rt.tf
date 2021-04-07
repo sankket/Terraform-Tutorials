6d76969cc380753f6eaad5c9382ed8600f0ae57a
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-04-07 20:05:39 +0530

    Code Updated

--- public-rt.tf
+++ public-rt.tf
@@ -1,1 +1,25 @@
-
+# Create a public route table for Public Subnets
+ 
+resource "aws_route_table" "dfsc-public" {
+  vpc_id = aws_vpc.dfsc_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dfsc-igw.id
+  }
+  tags = {
+    Name        = "DFSC Public Route Table"
+    Terraform   = "true"
+    }
+}
+ 
+# Attach a public route table to Public Subnets
+ 
+resource "aws_route_table_association" "dfsc-public-1a-association" {
+  subnet_id = aws_subnet.dfsc-public-1a.id
+  route_table_id = aws_route_table.dfsc-public.id
+}
+ 
+resource "aws_route_table_association" "dfsc-public-1b-association" {
+  subnet_id = aws_subnet.dfsc-public-1b.id
+  route_table_id = aws_route_table.dfsc-public.id
+}
