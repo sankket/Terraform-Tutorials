469ae9deb4bf0b5db7a9986e53118dbb10a4a4ba
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-04-07 19:17:28 +0530

    Code updated.

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,18 @@
+# Define VPC Variable
 
+variable "aws-vpc-cidr" {
+  type= string
+  #this is cidr block. it will have around 65k addresses.
+  default="10.0.0.0/16"
+}
+
+# Create VPC
+
+resource "aws_vpc" "dfsc_vpc" {
+  cidr_block = var.aws-vpc-cidr
+  instance_tenancy = "default"
+  tags = {
+    Name = "DFSC VPC"
+    Terrafrom = "True"
+  }
+}
