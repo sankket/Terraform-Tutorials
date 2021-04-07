b22760784fbcc6df256a3688a5e47eafda532109
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-04-07 22:36:19 +0530

    Code Updated

--- bastian-host.tf
+++ bastian-host.tf
@@ -1,1 +1,48 @@
+# Create Bastion Host Security Group
 
+resource "aws_security_group" "dfsc_bastion_sg" {
+  vpc_id = aws_vpc.dfsc_vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  tags = {
+    Name        = "DFSC Bastion Security Group"
+    Terraform   = "true"
+    } 
+}
+
+
+# CREATE BASTION HOST IN EU-WEST-1A PUBLIC SUBNET
+
+resource "aws_instance" "dfsc_bastion_host-1a" {
+  ami = "ami-0b4b2d87bdd32212a"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.ssh-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.dfsc_bastion_sg.id]
+  subnet_id = aws_subnet.dfsc-public-1a.id
+  tags = {
+    Name = "DFSC Bastion Host - 1A"
+    Terraform = true
+  }
+}
+
+# CREATE BASTION HOST IN EU-WEST-1B PUBLIC SUBNET
+
+resource "aws_instance" "dfsc_bastion_host-1b" {
+  ami = "ami-0b4b2d87bdd32212a"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.ssh-key.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.dfsc_bastion_sg.id]
+  subnet_id = aws_subnet.dfsc-public-1b.id
+  tags = {
+    Name = "DFSC Bastion Host - 1B"
+    Terraform = true
+  }
+}
