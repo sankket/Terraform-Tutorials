bcbef8ad32fcab36b80c2c15eda07354941413f2
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-04-07 19:51:31 +0530

    Code updated

--- subnets.tf
+++ subnets.tf
@@ -1,1 +1,48 @@
+# Create Public Subnets
 
+resource "aws_subnet" "dfsc-public-1a" {
+  vpc_id = aws_vpc.dfsc_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name        = "DFSC Pblic Subnet - 1A"
+    Terraform   = "True"
+  }
+}
+resource "aws_subnet" "dfsc-public-1b" {
+  vpc_id = aws_vpc.dfsc_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-west-1b"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name        = "DFSC Pblic Subnet - 1B"
+    Terraform   = "True"
+  }
+}
+
+
+# Create Private Subnets
+
+
+resource "aws_subnet" "dfsc-private-1a" {
+  vpc_id = aws_vpc.dfsc_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "eu-west-1a"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name        = "DFSC Private Subnet - 1A"
+    Terraform   = "True"
+  }
+}
+
+resource "aws_subnet" "dfsc-private-1b" {
+  vpc_id = aws_vpc.dfsc_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "eu-west-1b"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name        = "DFSC Private Subnet - 1B"
+    Terraform   = "True"
+  }
+}
